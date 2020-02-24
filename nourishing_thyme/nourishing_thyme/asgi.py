06ab809ec30c91a0d52930addf6e0dfd3ed48780
unknown <keegood8@gmail.com>
2020-02-24 11:11:12 -0800

    created nourishing thyme. deleted sample blog and users.py

--- nourishing_thyme/nourishing_thyme/asgi.py
+++ nourishing_thyme/nourishing_thyme/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sample_blog project.
+ASGI config for nourishing_thyme project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample_blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nourishing_thyme.settings')
 
 application = get_asgi_application()
