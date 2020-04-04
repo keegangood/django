e7577fb7b2d83acc204c1dc58b1290798a114473
unknown <keegood8@gmail.com>
2020-04-04 16:50:11 -0700

    migrate and test many to many relationship between pizzas and toppings

--- Signals_Pizza_Shop/toppings/models.py
+++ Signals_Pizza_Shop/toppings/models.py
@@ -1,10 +1,24 @@
 from django.db import models
+from pizzas.models import Pizza
+
+UNITS = [
+    ('oz', 'ounce'),
+    ('floz', 'fluid ounce'),
+    ('pc', 'piece'),
+    ('slc', 'slice')
+]
 
 class Topping(models.Model):
     name = models.CharField(
         max_length = 100,
-        required   = True,
-        unique     = True,
+        unique = True,
     )
 
-    
+    pizzas = models.ManyToManyField(Pizza)
+
+    quantity = models.DecimalField(max_digits=4, decimal_places=2)
+
+    units = models.CharField(max_length=4, choices=UNITS)
+
+    def __str__(self):
+        return f"{self.name} - {self.quantity}{self.units}"
