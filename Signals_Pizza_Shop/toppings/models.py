4209e791565e9ae5eec527773dc3a92d133cb662
Keegan Good <keegood8@gmail.com>
2020-04-04 15:42:56 -0700

    create pizzas model and toppings model

--- Signals_Pizza_Shop/toppings/models.py
+++ Signals_Pizza_Shop/toppings/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Topping(models.Model):
+    name = models.CharField(
+        max_length = 100,
+        required   = True,
+        unique     = True,
+    )
+
+    
